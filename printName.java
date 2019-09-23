471334e7034e7bd9dfe5ce1d5efef1c1189f012c
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-23 18:00:14 +0530

    testing

---
+++ printName.java
@@ -0,0 +1,7 @@
+//This Class is to Print a Name
+public class printName {
+    public static void main(String[] args) {
+        System.out.println("Sai Koteswara Rao Chinni");
+        //To print out Name
+    }
+}
