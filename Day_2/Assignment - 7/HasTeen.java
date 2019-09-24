6df18768c439d45e76bdd8f5b10cbb0917be918c
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-24 18:51:46 +0530

    Has Teen

--- Day_2/Assignment - 7/HasTeen.java
+++ Day_2/Assignment - 7/HasTeen.java
@@ -29,5 +29,10 @@
      */
     public static boolean hasTeen(int a, int b, int c) {
         // Your code goes here....
+        if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19)){
+            return true ;
+        }else {
+            return false ;
+        }
     }
 }
