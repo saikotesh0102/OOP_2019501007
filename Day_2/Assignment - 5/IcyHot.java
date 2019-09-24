56c5fdab2a3a91102fe8c97f5b002602c55a2f61
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-24 18:41:31 +0530

    Icy Hot

--- Day_2/Assignment - 5/IcyHot.java
+++ Day_2/Assignment - 5/IcyHot.java
@@ -28,5 +28,10 @@
      */
     public static boolean icyHot(int temp1, int temp2) {
         // Your code goes here...
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)){
+            return true ;
+        }else{
+            return false ;
+        }
     }
 }
