1640e7096e47e450831d069a111a46629a3df396
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-24 19:15:38 +0530

    Lone Teen

--- Day_2/Assignment - 8/LoneTeen.java
+++ Day_2/Assignment - 8/LoneTeen.java
@@ -27,5 +27,10 @@ public class LoneTeen {
      */
     public static boolean loneTeen(int a, int b) {
         // Your code goes here....
+        if (((a >= 13 && a <= 19) && (b >= 13 && b <= 19)) || ((!(a >= 13 && a <= 19) && !(b >= 13 && b <= 19)))){
+            return false ; 
+        }else {
+            return true ;
+        }
     }
 }
