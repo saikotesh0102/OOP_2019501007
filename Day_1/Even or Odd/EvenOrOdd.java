99c4ff66c257fc3bbeb2a2a025ea478771412b8d
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-24 14:07:00 +0530

    Area Of Circle

---
+++ Day_1/Even or Odd/EvenOrOdd.java
@@ -0,0 +1,19 @@
+/**
+ * Given n, return true if n is even otherwise false.
+ * 
+ * @author Siva Sankar
+ */
+
+public class EvenOrOdd {
+
+    /**
+     * This method returns true if it is an even number false otherwise.
+     * @param n, the integer
+     * @return true if n is even and false otherwise.
+     */
+    public static boolean evenOrOdd(int n) {
+        //  Your code goes here....
+        //  
+        return false;
+    }
+}
---
+++ Day_2/Practice Work/Test.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+public class Test {
+    public static void main(String[] args) {
+        //int a = 4;
+        //double b = 4.0;
+        //String c = "abc";
+        //String d = "abc";
+        //if (a == b) {
+            //System.out.println("True");
+        //}
+        //if (c.equals(d)) {
+        //    System.out.println("True");
+        //} else {
+        //    System.out.println("False");
+        //}
+        boolean isFalse = true;
+        if (isFalse) {
+            isFalse = true;
+        }else {
+            isFalse = false;
+        }
+        System.out.println(isFalse);
+    }
+}
