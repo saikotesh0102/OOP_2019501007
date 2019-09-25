9bc6274278a35aca925940fbbb96044c475719d2
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-25 14:24:16 +0530

    Squares

--- Day_3/Assignment - 3/Squares.java
+++ Day_3/Assignment - 3/Squares.java
@@ -23,7 +23,9 @@ public class Squares {
      */
     public static int[] squared(int[] nums) {
         //  Your code goes here.
-        
-        return null;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i] ;
+        }
+        return nums ;
     }
 }
