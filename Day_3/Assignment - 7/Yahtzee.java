dae65497077475f226e2c83437ecd2f575e64395
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-25 13:30:51 +0530

    Yahtzee

--- Day_3/Assignment - 7/Yahtzee.java
+++ Day_3/Assignment - 7/Yahtzee.java
@@ -1,29 +1,33 @@
 public class Yahtzee {
-    public static void main(String[] args) {
-        public int rollDice() {
-            double randomNumber = Math.random();
-            randomNumber = (randomNumber * 6) + 1 ;
-            int randomInt = (int) randomNumber ;
-            return randomInt ;
-        }
-        public int keepRolling(int dice1, int dice2, int dice3, int dice4, int dice5) {
+
+    public static int rollDice() {
+        double randomNumber = Math.random();
+        randomNumber = (randomNumber * 6) + 1 ;
+        int randomInt = (int) randomNumber ;
+        return randomInt ;
+    }
+    
+    public static int keepRolling() {
+        int count = 1 ;
+        int dice1 = rollDice() ;
+        int dice2 = rollDice() ;
+        int dice3 = rollDice() ;
+        int dice4 = rollDice() ;
+        int dice5 = rollDice() ;
+           
+        while(!(dice1 == dice2 && dice2 == dice3 && dice3 == dice4 && dice4 == dice5)){
             dice1 = rollDice() ;
             dice2 = rollDice() ;
             dice3 = rollDice() ;
             dice4 = rollDice() ;
-            dice5 = rollDice() ;
-            count = 1 ;
-           
-            while(!(dice1 == dice2 && dice2 == dice3 && dice3 == dice4 && dice4 == dice5)){
-                dice1 = rollDice() ;
-                dice2 = rollDice() ;
-                dice3 = rollDice() ;
-                dice4 = rollDice() ;
-                dice5 = rollDice() ; 
-                count = count + 1 ;
-            }
+            dice5 = rollDice() ; 
+            count = count + 1 ;
         }
         return count ;
-        keepRolling() ;  
     }
+
+    public static void main(String[] args) {
+        System.out.println(keepRolling()) ;
+    }
+    
 }
