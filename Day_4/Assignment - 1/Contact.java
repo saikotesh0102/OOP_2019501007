b9754af067397bb801fb0ce4ef59bf6bc707328c
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-25 21:06:01 +0530

    Stats

---
+++ Day_4/Assignment - 1/Contact.java
@@ -0,0 +1,12 @@
+/**
+ * This class discusses about the Contact Object. The attributes of the contact object
+ * are as follows.
+ * name : Name of the person / friend.
+ * email : mail id of the person / friend.
+ * phoneNumber : Phone Number of the person / friend.
+ * 
+ * @author Siva Sankar
+ */
+
+//  Your code goes here... For Contact class
+
