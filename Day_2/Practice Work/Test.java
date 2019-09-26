cfc51fc0caccb37ed919af128e13b07c436e0082
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-26 09:48:10 +0530

    Test

--- Day_2/Practice Work/Test.java
+++ Day_2/Practice Work/Test.java
@@ -1,15 +1,17 @@
 import java.lang.*;
 public class Test {
     public static void main(String[] args) {
-       int a = 10 ;
-        int b = a++ ;
-        System.out.println(a + " " + b) ;
-        a = b-- ;
-        System.out.println(a + " " + b) ;
-        a = b++ ;
-        System.out.println(a + " " + b) ;
-        b = b-- ;
-        a = a++ ;
-        System.out.println(a + " " + b) ;
+    //   int a = 10 ;
+    //    int b = a++ ;
+    //    System.out.println(a + " " + b) ;
+    //    a = b-- ;
+    //    System.out.println(a + " " + b) ;
+    //  a = b++ ;
+    //    System.out.println(a + " " + b) ;
+    //    b = b-- ;
+    //    a = a++ ;
+    //    System.out.println(a + " " + b) ;
+    int [] arr = new int[5] ;
+    System.out.println(arr);
     }
 }
