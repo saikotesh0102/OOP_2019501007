e3a7165f6cd75846610119c684dd479c4bc4f238
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-09-27 19:57:26 +0530

    Card Catalog

---
+++ Day_4/Assignment - 2/Card.java
@@ -0,0 +1,44 @@
+public class Card {
+    private String titleOfTheBook;
+    private String authorOfTheBook;
+    private String subjectOfTheBook;
+
+    public Card() {
+
+    }
+    public Card(final String titleOfTheBook, final String authorOftheBook, final String subjectOfTheBook) {
+        this.titleOfTheBook = titleOfTheBook;
+        this.authorOfTheBook = authorOftheBook;
+        this.subjectOfTheBook = subjectOfTheBook;
+    }
+
+    public void setTitleOfTheBook(final String titleOftheBook) {
+        this.titleOfTheBook = titleOftheBook;
+    }
+
+    public String getTitleOfTheBook() {
+        return this.titleOfTheBook;
+    }
+
+    public void setAuthorOfTheBook(final String authorOftheBook) {
+        this.authorOfTheBook = authorOftheBook;
+    }
+
+    public String getAuthorOfTheBook() {
+        return this.authorOfTheBook;
+    }
+
+    public void setSubjectOfTheBook(final String subjectOftheBook) {
+        this.subjectOfTheBook = subjectOftheBook;
+    }
+
+    public String getSubjectOfTheBook() {
+        return this.subjectOfTheBook;
+    }
+
+    public String toString() {
+        String output = "{ Name = " + this.titleOfTheBook + "," + " Author = "
+        + this.authorOfTheBook + "," + " Subject = " + this.subjectOfTheBook + " }";
+        return output;
+    }
+}
---
+++ Day_4/Assignment - 2/CardCatalog.java
@@ -0,0 +1,79 @@
+import java.util.*;
+// import java.io.BuffereedInputStream;
+
+public class CardCatalog {
+    int index ;
+    Card [] Catalog;
+
+    public CardCatalog(){
+        Catalog = new Card[10];
+        index = 0;
+    }
+
+    public Card getBookTitle(String title){
+        for(int i=0; i < Catalog.length; i++) {
+            if(Catalog[i].getTitleOfTheBook().equals(title)) {
+                return Catalog[i];
+            }
+        }return null;
+    }
+
+    public Card getBookAuthor(String author){
+        for(int i=0; i < Catalog.length; i++) {
+            if(Catalog[i].getAuthorOfTheBook().equals(author)) {
+                return Catalog[i];
+            }
+        }return null;
+    }
+
+    public Card getBookSubject(String subject){
+        for(int i=0; i < Catalog.length; i++) {
+            if(Catalog[i].getSubjectOfTheBook().equals(subject)) {
+                return Catalog[i];
+            }
+        }return null;
+    }
+
+    public void addCard(Card addition){
+        Catalog[index] = addition;
+        index++;
+    }
+
+    public void removeTitle(String removeTitle){
+        for(int i = 0; i < Catalog.length; i++){
+            if(Catalog[i].getTitleOfTheBook().equals(removeTitle)){
+                Catalog[i] = null;
+            }
+       }
+    }
+
+    public void printCatalog(){
+        for(int i = 0; i < Catalog.length;i++){
+            if(this.Catalog[i] != null){
+                System.out.println(Catalog[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Card firstCard = new Card();
+        firstCard.setAuthorOfTheBook("J.K.Rowling");
+        firstCard.setTitleOfTheBook("Harry Potter");
+        firstCard.setSubjectOfTheBook("Fiction");
+
+        Card secondCard = new Card();
+        secondCard.setAuthorOfTheBook("George R.R Martin");
+        secondCard.setTitleOfTheBook("A Song of Ice and Fire");
+        secondCard.setSubjectOfTheBook("Fantasy");
+
+        Card thirdCard = new Card();
+        thirdCard.setAuthorOfTheBook("Stephen Hawking");
+        thirdCard.setTitleOfTheBook("A Brief History of Time");
+        thirdCard.setSubjectOfTheBook("Science");
+
+        CardCatalog aCard = new CardCatalog();
+        aCard.addCard(firstCard);
+        aCard.addCard(secondCard);
+        aCard.addCard(thirdCard);
+        aCard.printCatalog();
+    }
+}
