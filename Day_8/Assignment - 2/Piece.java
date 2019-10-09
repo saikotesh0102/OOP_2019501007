4609cfeea7dcd035f8eb76311703499ed1f0cd1c
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-09 15:00:47 +0530

    Chess Game

--- Day_8/Assignment - 2/Piece.java
+++ Day_8/Assignment - 2/Piece.java
@@ -2,7 +2,7 @@ public class Piece {
     Position position;
 
     public boolean isValidMove(Position newPosition) {
-        if(newPosition.rowValue >= 0 && newPosition.columnValue >= 0
+        if(newPosition.rowValue > 0 && newPosition.columnValue > 0
         && newPosition.rowValue < 8 && newPosition.columnValue < 8) {
             return true;
         } else {
