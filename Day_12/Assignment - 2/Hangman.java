abef81a845e9a37e72f1778811b865de993ed6e4
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-10 20:10:29 +0530

    Hangman

--- Day_12/Assignment - 2/Hangman.java
+++ Day_12/Assignment - 2/Hangman.java
@@ -5,13 +5,16 @@ import java.io.*;
 public class Hangman{
     public static void main(String[] args) throws Exception {
         File file = new File("movies.txt");
-        // BufferedReader br = new BufferedReader(new FileReader(movies.txt));
-        Scanner scan = new Scanner(file);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        // Scanner scan = new Scanner(file);
 
-        String str = "";
-        while(scan.hasNextLine()){
-            str = str + scan.nextLine() + ",";
+        String str ;
+        // while(scan.hasNextLine()){
+        //     str = str + scan.nextLine() + ",";
+        // }
+        // System.out.println(str);
+        while ((str = br.readLine()) != null){
+            System.out.println(str); 
         }
-        System.out.println(str);
     }
 }
