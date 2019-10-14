17081dba967e718c973b254b87412ddf6c411c59
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-14 18:01:55 +0530

    Test

--- Day_2/Practice Work/Test.java
+++ Day_2/Practice Work/Test.java
@@ -1,22 +1,36 @@
 import java.lang.*;
 
 public class Test {
-    public Test(){
-        System.out.println("Base Show");
-    }
-}
+//     public Test(){
+//         System.out.println("Base Show");
+//     }
+// }
 
-public class Derived extends Test{
-    public Derived(){
-        System.out.println("Derived Show");
-    }
-}
+// public class Derived extends Test{
+//     public Derived(){
+//         System.out.println("Derived Show");
+//     }
+// }
 
-public class One{
+// public class One{
+//     public static void main(String[] args) {
+//         Test d = new Test();
+//         Derived b = new Derived();
+//         d.show();
+//         b.show(); 
+//     }
+    int a,b;
+    // public float add(int a, float b){
+    //     return a+b;
+    // }
+
+    public float add(float a, float b){
+        return a+b;
+    }
+
     public static void main(String[] args) {
-        Test d = new Test();
-        Derived b = new Derived();
-        d.show();
-        b.show(); 
+        Test T = new Test();
+        // System.out.println(T.add(10,11.5f));
+        System.out.println(T.add(10,6.5f));
     }
 }
