beae62bb34e09f8001d266c4a652d1964ef78969
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-14 19:56:50 +0530

    Exam

--- Exam/SocialNetwork.java
+++ Exam/SocialNetwork.java
@@ -3,6 +3,8 @@
  * You are free to add your own behaviours to this class.
  * 
  * @author Siva Sankar
+ * @author Sai Kotesh
+ * 
  */
 
 import java.util.*;
@@ -25,28 +27,138 @@ public class SocialNetwork {
     public SocialNetwork() {
         // TODO
         // Your code goes here
-        users = new User[10];
+        users = new User[1];
         size = 0;
     }
 
+    private void reSize() {
+        if (size == users.length) {
+            users = Arrays.copyOf(users, users.length + 1);
+        }
+    }
     /**
      * This method takes the string as a parameter which is used
      * to create a network. do nothing if the string is empty.
      * 
      * @param str to be used to create a network.
      */
+
+    private String[] increaseUserConOne(String[] cons, String str) {
+        if(cons != null && str != ""){
+            cons = Arrays.copyOf(cons, cons.length + 1);
+            cons[cons.length - 1] = str;
+            return cons;
+        } else {
+            String[] tempCons = new String[1];
+            tempCons[0] = str;
+            return tempCons;
+        }
+    }
+    private String[] increaseUserCon(String[] cons, String str) {
+        if(cons != null && str != ""){
+            String[] newStr = str.split(",");
+            cons = Arrays.copyOf(cons, cons.length + newStr.length);
+            for (int i = cons.length, j = 0; i < cons.length + newStr.length && j < newStr.length; i++, j++) {
+                cons[i] = newStr[j];
+            }
+            return cons;
+        }
+        return null;
+    }
+    
     public void createDataStructure(String str) {
         // TODO
         // Your code goes here
-        if(str.length() != 0){
-            String [] tokens = str.split("\r\n");
-            for(int i = 0 ; i < tokens.length ; i++){
-                String[] words = tokens[i].split(" ");
-                users[size].setUserName(words[0]);
-                users[size].setConnectionsOfTheUser(words[words.length-1].split(","));
-                size++;
+        if (str!= "") {
+            String[] eachUserStrings = str.split(";");
+            if(eachUserStrings.length != 0) {
+                for (int i = 0; i < eachUserStrings.length; i++) {
+                    String[] currentUser = eachUserStrings[i].split(" is connected to ");
+                    // System.out.println(" i " + i + " size " + size + " curr User " + currentUser[0]);
+                    if(currentUser.length == 2 ) {
+                            boolean currentUserPresent = false;
+
+                            for (int j = 0; j < size; j++) {
+                                if(users[j].userName.equals(currentUser[0])) {
+                                    currentUserPresent = true;
+                                    if(users[j].userConnections == null || users[j].userConnections.length == 0) {
+                                        String[] currentConnections = currentUser[1].split(",");
+                                        users[j].userConnections = currentConnections;
+                                    }
+                                    else {
+                                        users[j].userConnections = increaseUserCon(users[j].userConnections, currentUser[1]);
+                                    }
+                                    break;
+                                }
+                            }
+                            if (!currentUserPresent) {
+                                reSize();
+                                String[] yescons =  currentUser[1].split(",");
+                                User tempNewUser = new User(currentUser[0],yescons);
+                                users[size] = tempNewUser;
+                                size = size + 1;
+
+                            }
+                            // System.out.println("curr " +currentUser[0] + " pre? " +currentUserPresent);
+                            String[] currentUserConnections = currentUser[1].split(",");
+                            // System.out.println("cur con  " +currentUserConnectionsString);
+
+                            for (int k = 0; k < currentUserConnections.length; k++) {
+                                boolean conUserPresent = false;
+
+                                for (int m = 0; m < size; m++) {
+                                    if(currentUserConnections[k].equals(users[m].userName)) {
+                                        conUserPresent = true;
+                                        // reSize();
+                                        // User tempCurrentUserConnections = new User(currentUserConnections[k], null);
+                                        // users[size] = tempCurrentUserConnections;
+                                        break;
+                                    }
+                                }
+                                if(!conUserPresent) {
+                                    reSize();
+                                    User tempNewUser = new User(currentUserConnections[k],null);
+                                    users[size] = tempNewUser;
+                                    size = size + 1;
+                                }
+                            }
+                    }
+                    else {
+                        reSize();
+                        User tempNewUser = new User(currentUser[0], null);
+                        users[size] = tempNewUser;
+                        size = size + 1;
+                    }
+
+                    // for (int p = 0 ; p < size ; p ++ ) {
+                    //     System.out.print(users[p].userName + "   " + getCurrCon(users[p]));
+                    //     System.out.println();
+                    // }
+                    // System.out.println("----------------------------");
+
+                    // System.out.println(users[i].userName + "   " + users[i].userConnections.length);
+                }
+
             }
         }
+        // for (int i = 0 ; i < size ; i ++ ) {
+        //     System.out.println(users[i].userName + "   " +  getCurrCon(users[i]));
+        // }
+        return;
+    }
+
+    private String getCurrCon(User user) {
+        
+        String con = "";
+        if(user.userConnections == null) {
+            return "";
+        }
+
+        for(int i = 0; i<user.userConnections.length;i++){
+            con = con + user.userConnections[i]+", ";
+        }
+        con = con.substring(0, con.length()-2);
+        return con;
     }
 
     private boolean searchUser(User user) {
@@ -66,9 +178,9 @@ public class SocialNetwork {
      * @return the User object based on the userName. 
      * Returns null otherwise
      */
-    public User getUser(String userName) {
+    public User getUser(String userName1) {
         for (int i = 0; i < size; i++) {
-            if (userName.equals(users[i].getUserName())) {
+            if (userName1.equals(this.users[i].userName)) {
                 return users[i];
             }
         }
@@ -84,21 +196,27 @@ public class SocialNetwork {
     public void addUser(User userA) {
         // TODO
         // Your code goes here
-        if(size == users.length){
-            users = Arrays.copyOf(users,users.length + 10);
-        }
-
-        boolean flag = false;
-        for(int i = 0; i < size ; i++){
-            if(userA.equals(users[i])){
-                flag = true;
-                break;
+        
+        if (userA != null && userA.userName != "") {
+            boolean alreadyPresent = true;
+            int userIndex = -1;
+            for (int i = 0; i < size; i++) {
+                if(this.users[i].userName.equals(userA.userName)){
+                    alreadyPresent = false;
+                    userIndex = i;
+                    break;
+                }
             }
+            if(alreadyPresent) {
+                reSize();
+                this.users[size] = userA;
+                size = size + 1;
+            }
+            // else {
+            //     users[userIndex].userConnections = userA.userConnections;
+            // }
         }
-
-        if(!flag) {
-            users[size++] = userA;
-        }
+        return;
     }
 
     /**
@@ -116,23 +234,27 @@ public class SocialNetwork {
     public void addConnection(User userA, User userB) {
         // TODO
         // Your code goes here
-        boolean flagA = false;
-        boolean flagB = false;
 
-        for(int i = 0; i < size; i++){
-            if(users[i].equals(userA)) {
-                flagA = true; 
+        if (userA != null && userB != null) {
+            boolean userAPresent = false;
+            boolean userBPresent = false;
+            int userAIndex = -1;
+            int userBIndex = -1;
+
+            for (int i = 0; i < size; i++) {
+                if(userA.userName.equals(this.users[i].userName)) {
+                    userAPresent = true;
+                    userAIndex = i;
+                }
+                if(userA.userName.equals(this.users[i].userName)) {
+                    userBPresent = true;
+                    userBIndex = i;
+                }
             }
-        }
 
-        for(int i = 0; i < size; i++){
-            if(users[i].equals(userB)) {
-                flagB = true; 
+            if(userAPresent && userBPresent) {
+                users[userAIndex].userConnections = increaseUserConOne(userA.userConnections, userB.userName);
             }
-        }
-
-        if(flagA && flagB) {
-            userA.add(userB.getUserName());
         }
     }
 
@@ -149,24 +271,22 @@ public class SocialNetwork {
     public User[] getConnections(User userA) {
         // TODO
         // Your code goes here
-        User [] connectionUsers = new User[20];
-        int count = 20;
-        String[] connections = new String[10];
+        if (userA != null && userA.userName != "" && userA.userName != null && userA.userConnections != null){
+            String[] userAConnectionsString = userA.userConnections;
 
-        for (int i = 0; i < size; i++){
-            if (users[i].equals(userA)){
-                connections = users[i].getConnectionsOfTheUser();
+            for (int i = 0; i < userAConnectionsString.length; i++) {
+                userAConnectionsString[i] = userAConnectionsString[i].replaceAll(" ", "");
             }
-        }
 
-        for(int i = 0; i < connections.length; i++){
-            for(int j=0; j < size; j++){
-                if(users[j].getUserName().equals(connections[i])){
-                    connectionUsers[count++] = users[j];
-                }
+            User[] userAConnections = new User[userAConnectionsString.length];
+
+            for (int j = 0; j < userAConnectionsString.length; j++) {
+                userAConnections[j] = getUser(userAConnectionsString[j]);
             }
+
+            return userAConnections;
         }
-        return connectionUsers;
+        return null;
     }
 
     /**
@@ -183,46 +303,51 @@ public class SocialNetwork {
     public User[] getCommonConnections(User userA, User userB) {
         // TODO
         // Your code goes here
-        User[] commonUsers = new User[20];
-        int count = 0;
-        boolean flagA = false;
-        boolean flagB = false;
+        if (userA != null && userB != null && userA.userName != "" && userA.userName != null
+                 && userB.userName != "" && userB.userName != null && userA.userConnections != null && userB.userConnections != null) {
 
-        for(int i = 0; i < size ; i++){
-            if(flagA && flagB){
-                break;
-            }
-            if(users[i].equals(userA)){
-                flagA = true;
-            }
-            else if(users[i].equals(userB)){
-                flagB = true;
-            }
-        }
+            boolean userAPresent = false;
+            boolean userBPresent = false;
+            int userAIndex = -1;
+            int userBIndex = -1;
 
-        if(flagA && flagB) {
-            String[] conA = userA.getConnectionsOfTheUser();
-            String[] conB = userB.getConnectionsOfTheUser();
-            String[] common = new String[20];
-            int commonSize = 0;
-
-            for (int i = 0; i < userA.getNumberOfConnections(); i++){
-                for (int j = 0; j < userB.getNumberOfConnections(); i++){ 
-                    if(conA[i].equals(conB[j])){
-                        common[commonSize++] = userA.getUserName();
-                        break;
-                    }
+            for (int i = 0; i < size; i++) {
+                if(userA.userName.equals(users[i].userName)) {
+                    userAPresent = true;
+                    userAIndex = i;
                 }
             }
 
-            for (int i = 0; i < common.length; i++){
-                for(int j = 0; j < users.length; j++){
-                    if (users[j].getUserName().equals(common[i])){
-                        commonUsers[count++] = users[j];
-                    }
+            for (int i = 0; i < size; i++) {
+                if(userA.userName.equals(users[i].userName)) {
+                    userBPresent = true;
+                    userBIndex = i;
                 }
             }
-            return commonUsers;
+
+            if(userAPresent && userBPresent) {
+                String[] userAConnections = userA.userConnections;
+                String[] userBConnections = userB.userConnections;
+
+                String commonConnections = "";
+
+                    for (int i = 0; i < userAConnections.length; i++) {
+                        for (int j= 0; j< userBConnections.length; j++) {
+                            if(userAConnections[i].equals(userBConnections[j])) {
+                                commonConnections = commonConnections + userAConnections[i] + ",";
+                                break;
+                            }
+                        }
+                    }
+
+                    String[] commonConnectionsString = commonConnections.split(",");
+
+                    User[] commonConnectionsObj = new User[commonConnectionsString.length];
+                    for (int i = 0; i < commonConnectionsString.length; i++) {
+                        commonConnectionsObj[i] = getUser(commonConnectionsString[i]);
+                    }
+                return commonConnectionsObj;
+                }
         }
         return null;
     }
@@ -233,16 +358,28 @@ public class SocialNetwork {
      * @return the String version of the network, return an empty string
      * if there are no users in the network.
      */
+
     public String toString() {
-        if (this.size == 0) {
+        if (size == 0) {
             return "";
         }
-        StringBuffer sb = new StringBuffer();
-        int i = 0;
-        for (i = 0; i < size-1; i++) {
-            sb.append(users[i] + "\n");
+
+        String complete ="";
+
+        for (int i = 0 ; i < size ; i ++ ) {
+            String currCons = getCurrCon(users[i]);
+            if(currCons == "") {
+                complete = complete + users[i].userName + " : "  + "\n";
+            } 
+            else {
+                complete = complete + users[i].userName + " : " + "[" + currCons + "]" + "\n";
+            }
         }
-        sb.append(users[i]);
-        return sb.toString();
-    }
+        complete = complete.substring(0, complete.length()-1);
+        
+        // System.out.println(complete);
+   
+        return complete;
+    } 
+
 }
--- Exam/User.java
+++ Exam/User.java
@@ -1,71 +1,30 @@
-import java.lang.*;
 import java.util.*;
-
+/**
+ * @author Sai Kotesh
+ */
 public class User {
-    private String userName;
-    private User [] connectionsOfTheUser;
-    private int numberOfConnections;
 
-    // public User(){
-    //     this.userName = userName;
-    //     this.numberOfConnections = numberOfConnections;
-    // }
+    String userName;
+    String[] userConnections;
 
-    public User(String userName, String [] connectionsOfTheUser){
+    public User(String userName, String[] userConnections) {
         this.userName = userName;
-        this.connectionsOfTheUser = new User[10];
-    }
-
-    // public User(String userName, String [] connectionsOfTheUser){
-    //     this.userName = userName;
-    //     this.connectionsOfTheUser = new String[10];
-    // }
-
-    public void add(String str){
-        if(numberOfConnections == connectionsOfTheUser.length){
-            connectionsOfTheUser = Arrays.copyOf(connectionsOfTheUser, connectionsOfTheUser.length + 10);
-        }
-    }
-
-    public void setUserName(final String userName) {
-        this.userName = userName;
+        this.userConnections = userConnections;
     }
 
     public String getUserName() {
-        return this.userName;
+        return userName;
     }
 
-    public void setNumberOfConnections(final int numberOfConnections) {
-        this.numberOfConnections = numberOfConnections;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
-    public int getNumberOfConnections() {
-        return this.numberOfConnections;
+    public String[] getUserConnections() {
+        return userConnections;
     }
 
-    public void setConnectionsOfTheUser(final User [] connectionsOfTheUser) {
-        this.connectionsOfTheUser = connectionsOfTheUser;
+    public void setUserConnections(String[] userConnections) {
+        this.userConnections = userConnections;
     }
-
-    public User[] getConnectionsOfTheUser() {
-        return this.connectionsOfTheUser;
-    }
-
-    // public String toString(){
-    //     if(connectionsOfTheUser != null){
-    //         String str = "[";
-    //         for(int i = 0; i < connectionsOfTheUser.length; i++){
-    //             str = str + connectionsOfTheUser[i] ;
-    //             if(i != connectionsOfTheUser.length-1){
-    //                 str += ", ";
-    //             }
-    
-    //         }
-    //         str += "]";
-    //         return userName + " : "+ str;
-    //     }else{
-    //         return userName + " : "+ "";
-    //     }
-       
-    // }
 }
