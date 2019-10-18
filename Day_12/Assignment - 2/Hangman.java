fe0b0eba8b3bdceefdb4f9872fdab0b0b6447f7b
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-18 10:32:28 +0530

    Hangman

--- Day_12/Assignment - 2/Hangman.java
+++ Day_12/Assignment - 2/Hangman.java
@@ -85,7 +85,7 @@ public class Hangman{
             System.out.print("1 for Easy\n2 for Medium\n3 for Hard\n4 for Random\nEnter Number : ");
             String number = input.next();
             // if (number != "1" || number != "2" || number != "3" || number != "4"){
-            if (number == "1" || number == "2" || number == "3" || number == "4"){
+            if (number.equals("1") || number.equals("2") || number.equals("3") || number.equals("4")){
                 break;
             }else{
                 System.out.print("----------------------------\nNumber must be 1 or 2 or 3 or 4\n");
