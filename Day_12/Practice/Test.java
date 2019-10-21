579b37d9a4c2922066373772b4d131a5a714dbd9
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-21 21:49:40 +0530

    Test

---
+++ Day_12/Practice/Test.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public class Test{
+    public static void main(String[] args) {
+        File file = new File("movies.txt");
+        Scanner scan = new Scanner(file);
+
+        while(scan.hasNext()){
+            String str = scan.nextLine();
+            if(! str.equals(" ")){
+
+            }
+        }
+        scan.close();
+    }
+}
